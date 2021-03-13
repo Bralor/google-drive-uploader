30a1a58861f454eff7bd671a37ca7f731ce01c00
Matous <bralor92@email.cz>
2021-03-13 21:03:41 +0100

    Testing of splitting path added.
    src/test_file_sorter.py:
    + new test suite added

---
+++ src/test_file_sorter.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+from src.file_sorter import split_path, parse_path
+
+PATH_1 = ['00_introduction.ipynb']
+PATH_2 = ['01_intro_to_programming/00_introduction.ipynb']
+PATH_3 = ['materials/01_intro_to_programming/00_introduction.ipynb']
+
+
+class TestFileSorter:
+    def test_split_path(self):
+        pass
+
+    @pytest.mark.parametrize("path_1", PATH_1)
+    def test_parse_path_1(self, path_1):
+        splitted_path = path_1.split(os.path.sep)
+        assert parse_path(splitted_path) == {"notebook": "00_introduction.ipynb"}
+
+    @pytest.mark.parametrize("path_2", PATH_2)
+    def test_parse_path_2(self, path_2):
+        splitted_path = path_2.split(os.path.sep)
+        assert parse_path(splitted_path) == {
+            "lesson": "01_intro_to_programming",
+            "notebook": "00_introduction.ipynb"
+        }
+
+    @pytest.mark.parametrize("path_3", PATH_3)
+    def test_parse_path_3(self, path_3):
+        splitted_path = path_3.split(os.path.sep)
+        assert parse_path(splitted_path) == {
+            "root": "materials",
+            "lesson": "01_intro_to_programming",
+            "notebook": "00_introduction.ipynb"
+        }
+
