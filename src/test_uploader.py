b453e045c4de1058b9d7317fe5506e1e314258ac
Matous <bralor92@email.cz>
2021-03-14 21:07:23 +0100

    Empty test suite added.
    src/test_uploader.py:
    + testing functions added

---
+++ src/test_uploader.py
@@ -0,0 +1,14 @@
+import pytest
+from src.uploader import GDrive
+
+
+class TestGUploader:
+    def test_inspect_root(self):
+        pass
+
+    def test_inspect_folder(self):
+        pass
+
+    def test_save_list_content(self):
+        pass
+
