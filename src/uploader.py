bc21561ec6f5e54b2b31ad3ba9c1e99ae7033b08
Matous <bralor92@email.cz>
2021-03-17 14:55:42 +0100

    A few changes inside the 'uploader' module.
    + method 'find_notebook' is searching for proper 'ipynb' notebook
    
    + method 'find_lesson' is searching for proper Drive directory. If it
      does not exist creates a new one.
    
    + method 'create_folder' is creating a new folder when the 'find_lesson'
      is evaluated as false.

--- src/uploader.py
+++ src/uploader.py
@@ -7,13 +7,11 @@ from pydrive2.drive import GoogleDrive
 
 
 class GDrive:
-    def __init__(self):
-        self.files = ["05_languages.png", "python_inst.png"]
-        # self.creds = "client_secrets.json"
-
+    def __init__(self, dirname):
         gauth = GoogleAuth()
         gauth.LocalWebserverAuth()
         self.drive = GoogleDrive(gauth)
+        self.dirname = dirname
 
     def inspect_root(self):
         return self.drive.ListFile(
@@ -25,18 +23,29 @@ class GDrive:
 
     def upload_files(self, destination, file):
         new_file = self.drive.CreateFile({
-            "title": file.split(os.path.sep)[1],
+            "title": os.path.basename(file),
             "parents": [{"id": destination}]
         })
         new_file.SetContentFile(file)
         new_file.Upload()
-        print(f"title:{new_file['title']}, id: {new_file['id']}")
+        print(f"Title:{new_file['title']}, id: {new_file['id']}")
 
     def update_files(self, destination, file):
         new_file = self.drive.CreateFile({'id': destination})
         new_file.SetContentFile(file)
         new_file.Upload()
         print(f"title:{new_file['title']}, id: {new_file['id']}")
+
+    def create_folder(self, parent_folder_id, subfolder_name):
+        new_folder = self.drive.CreateFile({
+            'title': subfolder_name,
+            "parents": [{
+                "kind": "drive#fileLink",
+                "id": parent_folder_id}],
+            "mimeType": "application/vnd.google-apps.folder"
+        })
+        new_folder.Upload()
+        return new_folder
 
     @staticmethod
     def save_list_content(iterable):
@@ -45,57 +54,22 @@ class GDrive:
             for file in iterable
         }
 
-
-if __name__ == "__main__":
-    upload = GDrive()
-    inspect_root = upload.inspect_root()
-    root_dict = upload.save_list_content(inspect_root)
-    # pprint(root_dict)
-
-    dir_engeto = upload.inspect_folder(root_dict.get("engeto_collabs"))
-    lessons = upload.save_list_content(dir_engeto)  # 01_lesson, 02_lesson
-    # pprint(lessons)
-
-    all_lessons = []
-    for lesson in lessons:
-        dir_lesson = upload.inspect_folder(lessons.get(lesson))
-        dict_of_files = upload.save_list_content(dir_lesson)
-        all_lessons.append(dict_of_files)
-    # pprint(all_lessons)
-
-    def content_checker(content, file):
+    @staticmethod
+    def find_notebook(content, notebook):
         for lesson in content:  # 01_lesson, 02_lesson
             for chapter in lesson:  # 00_introduction, 01_..., notes.txt
-                if chapter == file:
+                if chapter == notebook:
                     return lesson.get(chapter)
                 else:
                     continue
         return False
 
-
-    tested_status_3 = {
-        'samples/notes.txt': {
-            'lesson': '01_lesson',
-            'notebook': 'notes.txt'
-        }
-    }
-
-    tested_files = [tested_status_3]
-
-    for tested_file in tested_files:  # "notes.txt"
-        file_path = (list(tested_file.keys())[0]) # "samples/notes.txt"
-        notebook = tested_file.get(file_path).get("notebook")  # "notes.txt"
-
-        if (file_id := content_checker(all_lessons, notebook)):  # False
-            if notebook:  # 00_introduction.ipynb
-                lesson = tested_file.get(file_path).get("lesson")  # 01_intro_to_programming
-                print("Uploading and updating file")
-                upload.update_files(file_id, file_path)
-
-        else:
-            if notebook: # True
-                lesson = tested_file.get(file_path).get("lesson")  # 01_lesson
-                remote_dir_id = lessons.get(lesson)  # '1x8fkMqA5u8lo0WMs0GKeZheG-fLxXjeT'
-                print("Just uploading file..")
-                upload.upload_files(remote_dir_id, file_path)
+    @staticmethod
+    def find_lesson(content, folder):
+        for dir_c in content:    # 01_intro_to_programming, 02_conditions
+            if dir_c == folder:  # "01_intro_to_programming" == "01_intro_to_programming"
+                return content.get(dir_c)
+            else:
+                continue
+        return False
 
